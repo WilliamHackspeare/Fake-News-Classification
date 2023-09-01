f88dfd86a2cae4c10ed1abaaf6339de60f4f101b
Amritanshu Prasad <amritanshuprasad2001@gmail.com>
2023-09-01 15:21:12 +0530

    Update Fake_News_Classification.py

--- Fake_News_Classification.py
+++ Fake_News_Classification.py
@@ -1,32 +1,54 @@
-import pandas as pd                                             #Pandas library allows us to process and analyse extensive data with ease, using special data structures and functions
-                                                                #Scikit-learn or sklearn is an important ML package for Python.
-from sklearn.model_selection import train_test_split            #Import the train_test_split function, which splits the labelled dataset into a random training and testing dataset to allow cross validation
-from sklearn.feature_extraction.text import TfidfVectorizer     #Import the TfidfVectorizer which converts the dataset into a matrix of TF-IDF features
-from sklearn.linear_model import PassiveAggressiveClassifier    #Import the PassiveAggressiveClassifier which is an online learning classification algorithm
-from sklearn.metrics import accuracy_score                      #Import the accuracy_score function which provides a metric as to the accuracy of the algorithm
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import re
 
-df=pd.read_csv('C:\\news.csv')                                                                          #Read the training dataset
-df.shape                                                                                                #Get shape and head
-labels=df.label                                                                                         #Get the labels
-runs = 43                                                                                               #Number of iterations to run on the data
-avg_acc = 0
-avg_fake = 0
+# Load the dataset
+df = pd.read_csv("C:\\Users\\amrit\\Downloads\\fake_or_real_news.csv")
 
-for rs in range(runs):
-    x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=rs)  #Split the dataset
-    tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)                                  #Initialize a TfidfVectorizer
-    tfidf_train=tfidf_vectorizer.fit_transform(x_train)                                                 #Fit and transform train set
-    tfidf_test=tfidf_vectorizer.transform(x_test)                                                       #Transform test set
-    pac=PassiveAggressiveClassifier(max_iter=50)                                                        #Initialize a PassiveAggressiveClassifier
-    pac.fit(tfidf_train,y_train)                                                                        #Fit the classifier on the transformed training set and the original set
-    pred=pac.predict(tfidf_test)                                                                        #Predict on the test set
-    score=accuracy_score(y_test,pred)*100                                                               #Calculate the accuracy                                                                 
-    avg_acc += score/runs                                                                               
-    df2=pd.read_csv('GRD.csv')                                                                          #Read the dataset to be analysed
-    tfidf_pred=tfidf_vectorizer.transform(df2['text'])                                                  #Transform the final dataset
-    pred=pac.predict(tfidf_pred)                                                                        #Predict on the final set
-    fin = list(pred)                                                                                    #Save the predictions as a list
-    fake = ((fin.count("FAKE")/len(fin))*100)                                                           #Calculate the percentage of fake news
-    avg_fake += fake/runs
-print("Average accuracy:",avg_acc,"%")                                                                  #Print the average accuracy of the classifier
-print("Percentage of fake news:",avg_fake,"%")                                                          #Print the percentage of fake news on average
+# Initialize WordNet Lemmatizer and stopwords
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+# Function to clean text
+def clean_text(text):
+    # Remove special characters
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    # Convert to lowercase
+    text = text.lower()
+    # Tokenize
+    words = text.split()
+    # Remove stopwords and apply lemmatization
+    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+    # Join the words back into a string
+    text = ' '.join(words)
+    return text
+
+# Apply text cleaning
+df['cleaned_text'] = df['text'].apply(clean_text)
+
+# Feature extraction using TF-IDF
+tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+X = tfidf_vectorizer.fit_transform(df['cleaned_text']).toarray()
+y = df['label']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the Random Forest Classifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+# Predict the labels for the test set
+y_pred = clf.predict(X_test)
+
+# Evaluate the model
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
